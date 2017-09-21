a0c936974b4f7742a75b64dc193f308b9078eede
Yuval Saraf <unimonkiez@gmail.com>
2017-09-21 21:39:07 +0300

    Added offline mode for node-api

--- src/common/node-api.ts
+++ src/common/node-api.ts
@@ -1,5 +1,29 @@
 const serverUrl = 'http://localhost:5000';
 
+/* START OF FOR DEVELOPMENT - NO NEED FOR PYTHON SERVER */
+// export const getRoot = () => ({
+//     then: cb => {
+//         setTimeout(() => {
+//             cb('root');
+//         }, 500);
+//         return {
+//             catch(cb2) {}
+//         }
+//     }
+// });
+// let counter = 0;
+// export const getListByParentId = id => ({
+//     then: cb => {
+//         setTimeout(() => {
+//             cb([++counter, ++counter]);
+//         }, 500);
+//         return {
+//             catch(cb2) { }
+//         }
+//     }
+// });
+/* END OF FOR DEVELOPMENT - NO NEED FOR PYTHON SERVER */
+
 export const getRoot = () => fetch(`${serverUrl}/root`)
 .then(res => res.json())
 .then(res => res.root);
